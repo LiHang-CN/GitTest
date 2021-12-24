29c0b63fdbddd58c758026c1b3b68f115fbef62b
李杭 <lihang_1970@163.com>
2021-12-24 16:29:36 +0800

    commit

---
+++ src/main/java/com/lihang/gittest/Hello.java
@@ -0,0 +1,13 @@
+package com.lihang.gittest;
+
+/**
+ * @author : LiHang
+ * @description :
+ * @date : Created in 2021/12/24 16:28
+ * @version: 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
